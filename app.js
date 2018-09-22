078ee686612384de4364d19ab004b9a0b5d5ffc8
Maksym Shutiak <max@rademade.com>
2018-09-22 20:02:11 +0300

    Update CORS

--- app.js
+++ app.js
@@ -6,41 +6,19 @@ let expressValidator = require('express-validator');
 let routes = require('./routes');
 let cors = require('cors');
 const session = require('express-session');
-const MemcachedStore = require('connect-memjs')(session);
 const passport = require('passport');
 const config = require('./config');
 
 let app = express();
-const sessionConfig = {
-  resave: false,
-  saveUninitialized: false,
-  secret: config.get('SECRET'),
-  signed: true
-};
-
-// In production use the Memcache instance to store session data,
-// otherwise fallback to the default MemoryStore in development.
-if (config.get('NODE_ENV') === 'production' && config.get('MEMCACHE_URL')) {
-  if (config.get('MEMCACHE_USERNAME') && (config.get('MEMCACHE_PASSWORD'))) {
-    sessionConfig.store = new MemcachedStore({
-      servers: [config.get('MEMCACHE_URL')],
-      username: config.get('MEMCACHE_USERNAME'),
-      password: config.get('MEMCACHE_PASSWORD')});
-  } else {
-    sessionConfig.store = new MemcachedStore({
-      servers: [config.get('MEMCACHE_URL')]
-    });
-  }
-}
 let corsOptions = {};
-let whitelist = ['https://human-resources-web.firebaseapp.com'];
+let whitelist = ['https://human-resources-web.firebaseapp.com','http://localhost:3000'];
 if (process.env.NODE_ENV === 'production') {
   corsOptions = {
     origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, false)
+      if (whitelist.indexOf('https://human-resources-web.firebaseapp.com') || whitelist.indexOf('http://localhost:3000')) {
+        callback(null, false);
       } else {
-        callback(new Error('Not allowed by CORS'))
+        callback(new Error('Not allowed by CORS'));
       }
     }
   };
@@ -55,7 +33,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session(sessionConfig));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(require('./routes/oauth2').router);
