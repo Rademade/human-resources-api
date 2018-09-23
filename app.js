86de2e3a0c119079bdd6de2814424b32fccc5c76
Maksym Shutiak <max@rademade.com>
2018-09-23 14:43:39 +0300

    Test CORS

--- app.js
+++ app.js
@@ -10,18 +10,7 @@ const passport = require('passport');
 // const config = require('./config');
 
 let app = express();
-let whitelist = ['https://human-resources-web.firebaseapp.com', 'http://localhost:8080'];
-let corsOptionsDelegate = function (req, callback) {
-  let corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-};
 app.use(logger('dev'));
-app.use(cors());
 app.use(expressValidator());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,10 +19,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(require('./routes/oauth2').router);
+app.use(cors({
+  origin: 'https://human-resources-web.firebaseapp.com'
+}));
 
 app.use('/index', function (req,res) {
   res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
-app.use('/api', cors(corsOptionsDelegate), routes);
+app.use('/api', cors(),routes);
 
 module.exports = app;
